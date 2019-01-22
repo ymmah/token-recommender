2919103e1ec4a668940ce69c021c7a836031aad0
Evgeny Medvedev <medvedev1088@gmail.com>
2019-01-22 15:28:51 +0700

    Add TokenService

--- app/recommendations.py
+++ app/recommendations.py
@@ -22,6 +22,8 @@ import pandas as pd
 import google.auth
 import google.cloud.storage as storage
 
+from token_service import TokenService
+
 logging.basicConfig(level=logging.INFO)
 
 LOCAL_MODEL_PATH = '/tmp'
@@ -34,134 +36,149 @@ USER_TOKEN_DATA_FILE = 'data/token_balances.csv'
 
 
 class Recommendations(object):
-  """Provide recommendations from a pre-trained collaborative filtering model.
-
-  Args:
-    local_model_path: (string) local path to model files
-  """
-
-  def __init__(self, local_model_path=LOCAL_MODEL_PATH):
-    _, project_id = google.auth.default()
-    self._bucket = 'recserve_' + project_id
-    self._load_model(local_model_path)
-
-  def _load_model(self, local_model_path):
-    """Load recommendation model files from GCS.
+    """Provide recommendations from a pre-trained collaborative filtering model.
 
     Args:
       local_model_path: (string) local path to model files
     """
-    # download files from GCS to local storage
-    os.makedirs(os.path.join(local_model_path, 'model'), exist_ok=True)
-    os.makedirs(os.path.join(local_model_path, 'data'), exist_ok=True)
-    client = storage.Client()
-    bucket = client.get_bucket(self._bucket)
 
-    logging.info('Downloading blobs.')
+    def __init__(self, local_model_path=LOCAL_MODEL_PATH):
+        _, project_id = google.auth.default()
+        self._bucket = 'recserve_' + project_id
+        self._load_model(local_model_path)
+        self._token_service = TokenService()
 
-    model_files = [ROW_MODEL_FILE, COL_MODEL_FILE, USER_MODEL_FILE,
-                   ITEM_MODEL_FILE, USER_TOKEN_DATA_FILE]
-    for model_file in model_files:
-      blob = bucket.blob(model_file)
-      with open(os.path.join(local_model_path, model_file), 'wb') as file_obj:
-        blob.download_to_file(file_obj)
+    def _load_model(self, local_model_path):
+        """Load recommendation model files from GCS.
 
-    logging.info('Finished downloading blobs.')
+        Args:
+          local_model_path: (string) local path to model files
+        """
+        # download files from GCS to local storage
+        os.makedirs(os.path.join(local_model_path, 'model'), exist_ok=True)
+        os.makedirs(os.path.join(local_model_path, 'data'), exist_ok=True)
+        client = storage.Client()
+        bucket = client.get_bucket(self._bucket)
 
-    # load npy arrays for user/item factors and user/item maps
-    self.user_factor = np.load(os.path.join(local_model_path, ROW_MODEL_FILE))
-    self.item_factor = np.load(os.path.join(local_model_path, COL_MODEL_FILE))
-    self.user_map = np.load(os.path.join(local_model_path, USER_MODEL_FILE))
-    self.item_map = np.load(os.path.join(local_model_path, ITEM_MODEL_FILE))
+        logging.info('Downloading blobs.')
 
-    logging.info('Finished loading arrays.')
+        model_files = [ROW_MODEL_FILE, COL_MODEL_FILE, USER_MODEL_FILE,
+                       ITEM_MODEL_FILE, USER_TOKEN_DATA_FILE]
+        for model_file in model_files:
+            blob = bucket.blob(model_file)
+            with open(os.path.join(local_model_path, model_file), 'wb') as file_obj:
+                blob.download_to_file(file_obj)
 
-    # load user_item history into pandas dataframe
-    views_df = pd.read_csv(os.path.join(local_model_path,
-                                        USER_TOKEN_DATA_FILE), sep=',', header=0)
-    self.user_items = views_df.groupby('user_address')
+        logging.info('Finished downloading blobs.')
 
-    logging.info('Finished loading model.')
+        # load npy arrays for user/item factors and user/item maps
+        self.user_factor = np.load(os.path.join(local_model_path, ROW_MODEL_FILE))
+        self.item_factor = np.load(os.path.join(local_model_path, COL_MODEL_FILE))
+        self.user_map = np.load(os.path.join(local_model_path, USER_MODEL_FILE))
+        self.item_map = np.load(os.path.join(local_model_path, ITEM_MODEL_FILE))
 
-  def get_recommendations(self, user_address, num_recs):
-    """Given a user id, return list of num_recs recommended item ids.
+        logging.info('Finished loading arrays.')
 
-    Args:
-      user_address: (string) The user address
-      num_recs: (int) The number of recommended items to return
+        # load user_item history into pandas dataframe
+        views_df = pd.read_csv(os.path.join(local_model_path,
+                                            USER_TOKEN_DATA_FILE), sep=',', header=0)
+        self.user_items = views_df.groupby('user_address')
 
-    Returns:
-      [item_id_0, item_id_1, ... item_id_k-1]: The list of k recommended items,
-        if user id is found.
-      None: The user id was not found.
-    """
-    token_recommendations = None
+        logging.info('Finished loading model.')
 
-    # map user address into ratings matrix user index
-    user_idx = np.searchsorted(self.user_map, user_address)
+    def get_recommendations(self, user_address, num_recs):
+        """Given a user id, return list of num_recs recommended item ids.
 
-    if user_idx:
-      # get already viewed items from views dataframe
-      try:
-        already_rated = self.user_items.get_group(user_address).token_address
-      except KeyError:
-        already_rated = []
+        Args:
+          user_address: (string) The user address
+          num_recs: (int) The number of recommended items to return
 
-      already_rated_idx = [np.searchsorted(self.item_map, i)
-                           for i in already_rated]
+        Returns:
+          [item_id_0, item_id_1, ... item_id_k-1]: The list of k recommended items,
+            if user id is found.
+          None: The user id was not found.
+        """
+        token_address_recommendations = []
 
-      # generate list of recommended article indexes from model
-      recommendations = generate_recommendations(user_idx, already_rated_idx,
-                                                 self.user_factor,
-                                                 self.item_factor,
-                                                 num_recs)
+        # map user address into ratings matrix user index
+        user_idx = np.searchsorted(self.user_map, user_address)
 
-      # map article indexes back to article ids
-      token_recommendations = [self.item_map[i] for i in recommendations]
+        if user_idx:
+            # get already viewed items from views dataframe
+            try:
+                already_rated = self.user_items.get_group(user_address).token_address
+            except KeyError:
+                already_rated = []
 
-    return token_recommendations
+            already_rated_idx = [np.searchsorted(self.item_map, i)
+                                 for i in already_rated]
+
+            # generate list of recommended article indexes from model
+            recommendations = generate_recommendations(user_idx, already_rated_idx,
+                                                       self.user_factor,
+                                                       self.item_factor,
+                                                       num_recs)
+
+            # map article indexes back to article ids
+            token_address_recommendations = [self.item_map[i] for i in recommendations]
+
+        token_recommendations = self.map_to_tokens(token_address_recommendations)
+
+        return token_recommendations
+
+    def map_to_tokens(self, token_addresses):
+        result = []
+        for address in token_addresses:
+            token = self._token_service.get_token_by_address(address)
+            if token is None:
+                token = {
+                    'address': address,
+                    'symbol': 'Unknown',
+                    'name': 'Unknown'
+                }
+            result.append(token)
+        return result
 
 
 def generate_recommendations(user_idx, user_rated, row_factor, col_factor, k):
-  """Generate recommendations for a user.
+    """Generate recommendations for a user.
 
-  Args:
-    user_idx: the row index of the user in the ratings matrix,
+    Args:
+      user_idx: the row index of the user in the ratings matrix,
 
-    user_rated: the list of item indexes (column indexes in the ratings matrix)
-      previously rated by that user (which will be excluded from the
-      recommendations),
+      user_rated: the list of item indexes (column indexes in the ratings matrix)
+        previously rated by that user (which will be excluded from the
+        recommendations),
 
-    row_factor: the row factors of the recommendation model
+      row_factor: the row factors of the recommendation model
 
-    col_factor: the column factors of the recommendation model
+      col_factor: the column factors of the recommendation model
 
-    k: number of recommendations requested
+      k: number of recommendations requested
 
-  Returns:
-    list of k item indexes with the predicted highest rating,
-    excluding those that the user has already rated
-  """
+    Returns:
+      list of k item indexes with the predicted highest rating,
+      excluding those that the user has already rated
+    """
 
-  # bounds checking for args
-  assert (row_factor.shape[0] - len(user_rated)) >= k
+    # bounds checking for args
+    assert (row_factor.shape[0] - len(user_rated)) >= k
 
-  # retrieve user factor
-  user_f = row_factor[user_idx]
+    # retrieve user factor
+    user_f = row_factor[user_idx]
 
-  # dot product of item factors with user factor gives predicted ratings
-  pred_ratings = col_factor.dot(user_f)
+    # dot product of item factors with user factor gives predicted ratings
+    pred_ratings = col_factor.dot(user_f)
 
-  # find candidate recommended item indexes sorted by predicted rating
-  k_r = k + len(user_rated)
-  candidate_items = np.argsort(pred_ratings)[-k_r:]
+    # find candidate recommended item indexes sorted by predicted rating
+    k_r = k + len(user_rated)
+    candidate_items = np.argsort(pred_ratings)[-k_r:]
 
-  # remove previously rated items and take top k
-  recommended_items = [i for i in candidate_items if i not in user_rated]
-  recommended_items = recommended_items[-k:]
+    # remove previously rated items and take top k
+    recommended_items = [i for i in candidate_items if i not in user_rated]
+    recommended_items = recommended_items[-k:]
 
-  # flip to sort highest rated first
-  recommended_items.reverse()
+    # flip to sort highest rated first
+    recommended_items.reverse()
 
-  return recommended_items
-
+    return recommended_items
---
+++ app/token_service.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+import google.auth
+import google.cloud.storage as storage
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+
+LOCAL_PATH = '/tmp'
+
+TOKENS_FILE = 'data/tokens.csv'
+
+
+class TokenService(object):
+
+    def __init__(self, local_model_path=LOCAL_PATH):
+        _, project_id = google.auth.default()
+        self._bucket = 'recserve_' + project_id
+        self._load_tokens(local_model_path)
+
+    def _load_tokens(self, local_model_path):
+        # download files from GCS to local storage
+        os.makedirs(os.path.join(local_model_path, 'data'), exist_ok=True)
+        client = storage.Client()
+        bucket = client.get_bucket(self._bucket)
+
+        logging.info('Downloading blobsf for TokenService.')
+
+        blob = bucket.blob(TOKENS_FILE)
+        with open(os.path.join(local_model_path, TOKENS_FILE), 'wb') as file_obj:
+            blob.download_to_file(file_obj)
+
+        logging.info('Finished downloading blobs for TokenService.')
+
+        # load tokens
+        tokens_df = pd.read_csv(os.path.join(local_model_path, TOKENS_FILE), sep=',', header=0)
+        tokens_df['address_index'] = tokens_df['address']
+        tokens_df.set_index('address_index', inplace=True)
+        self.tokens = tokens_df.to_dict('index')
+
+    def get_token_by_address(self, address):
+        return self.tokens.get(address)
