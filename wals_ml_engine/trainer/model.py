a828ba33b6d7d3283a6feba7a90aedbc17cce778
Evgeny Medvedev <medvedev1088@gmail.com>
2019-01-22 12:56:47 +0700

    Fix header

--- wals_ml_engine/trainer/model.py
+++ wals_ml_engine/trainer/model.py
@@ -85,7 +85,7 @@ def _token_balances_train_and_test(input_file):
   balances_df = pd.read_csv(input_file,
                            sep=',',
                            names=headers,
-                           header=0,
+                           header=None,
                            dtype={
                                'token_address': np.str,
                                'user_address': np.str,
