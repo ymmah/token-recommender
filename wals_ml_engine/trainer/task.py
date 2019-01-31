e73f2182edb9394c952eb0e04e5cafaab28eb073
Evgeny Medvedev <medvedev1088@gmail.com>
2019-02-01 00:35:35 +0700

    Remove unused code

--- wals_ml_engine/trainer/task.py
+++ wals_ml_engine/trainer/task.py
@@ -17,127 +17,28 @@
 import argparse
 import json
 import os
-import pickle
 
-import scipy.sparse as sp
 import tensorflow as tf
-from lightfm import LightFM
-from lightfm.evaluation import precision_at_k, recall_at_k
-from pandas import Series, np
-from tqdm import tqdm
 
 import model
 import util
-import wals
-
-
-def train_and_evaluate_wals(args):
-    # process input file
-    input_file = util.ensure_local_file(args['train_files'][0])
-    user_map, item_map, tr_sparse, test_sparse = model.create_test_and_train_sets(input_file)
-
-    # train model
-    output_row, output_col = model.train_model(args, tr_sparse)
-
-    # save trained model to job directory
-    model.save_model(args, user_map, item_map, output_row, output_col)
-
-    # log results
-    train_rmse = wals.get_rmse(output_row, output_col, tr_sparse)
-    test_rmse = wals.get_rmse(output_row, output_col, test_sparse)
-
-    precision, recall, pop_precision, pop_recall = wals.get_precision_recall(tr_sparse,
-                                                                             test_sparse,
-                                                                             output_row,
-                                                                             output_col,
-                                                                             5)
-
-    if args['hypertune']:
-        # write test_rmse metric for hyperparam tuning
-        util.write_hptuning_metric(args, test_rmse)
-
-    tf.logging.info('train RMSE = %.2f' % train_rmse)
-    tf.logging.info('test RMSE = %.2f' % test_rmse)
-
-    tf.logging.info('test precision = %.2f%%' % (100 * precision))
-    tf.logging.info('test recall = %.2f%%' % (100 * recall))
-
-    tf.logging.info('popularity precision = %.2f%%' % (100.0 * pop_precision))
-    tf.logging.info('popularity recall = %.2f%%' % (100.0 * pop_recall))
 
 
 def train_and_evaluate_lightfm(args):
-    k = 5
-    no_components = 11
-
     input_file = util.ensure_local_file(args['train_files'][0])
 
     print("Creating datasets ...")
     users, items, train, test = model.create_test_and_train_sets(input_file)
 
     print("Training model ...")
-    lfm = LightFM(loss='warp', no_components=no_components, learning_schedule='adagrad')
-    lfm.fit(train, epochs=20, num_threads=2)
+    mdl = model.train_model(args, train)
 
-    job_dir = args['job_dir']
-    print("Saving model to {} ...".format(job_dir))
-    if not os.path.exists(job_dir):
-        os.makedirs(job_dir)
-    with open(os.path.join(job_dir, 'model.pickle'), 'wb') as output_file:
-        output_file.write(pickle.dumps(lfm))
-    np.save(os.path.join(job_dir, 'user'), users)
-    np.save(os.path.join(job_dir, 'item'), items)
+    print("Saving model ...")
+    model.save_model(args, mdl, users, items)
 
     print("Evaluating model ...")
-    test_precision = precision_at_k(lfm, test, train, k=k).mean()
-    print("test precision@%d: %.2f%%" % (k, test_precision * 100.0))
-    test_recall = recall_at_k(lfm, test, train, k=k).mean()
-    print("test recall@%d: %.2f%%" % (k, test_recall * 100.0))
+    model.evaluate_model(args, mdl, train, test)
 
-    train_precision = precision_at_k(lfm, train, k=k).mean()
-    print("train precision@%d: %.2f%%" % (k, train_precision * 100.0))
-    train_recall = recall_at_k(lfm, train, k=k).mean()
-    print("train recall@%d: %.2f%%" % (k, train_recall * 100.0))
-
-    if args.get('eval_popularity') == 'True':
-        pop_precision = precision_at_k(popularity_model(k=k), test, train, k=k).mean()
-        print("popularity precision@%d: %.2f%%" % (k, pop_precision * 100.0))
-        pop_recall = recall_at_k(popularity_model(k=k), test, train, k=k).mean()
-        print("popularity recall@%d: %.2f%%" % (k, pop_recall * 100.0))
-
-
-def popularity_model(k=10):
-    def get_most_popular(sparse):
-        """Get the k most popular items in sparse based on number of ratings."""
-        return list(Series(sparse.nonzero()[1]).value_counts()[:(k*10)].index)
-
-    class PopularityModel:
-        def predict_rank(self, test_interactions, train_interactions, **kwargs):
-            most_popular = get_most_popular(test_interactions)
-            num_users = test_interactions.shape[0]
-            data = []
-            rows = []
-            cols = []
-            for user in tqdm(range(0, num_users)):
-                test_items = set(test_interactions.getrow(user).indices)
-                train_items = set(train_interactions.getrow(user).indices)
-                recommended_items = [item for item in most_popular if item not in train_items]
-                predictions = set(recommended_items).intersection(test_items)
-
-                recommended_ranks = {item: index for index, item in enumerate(recommended_items)}
-
-                for prediction in predictions:
-                    rank = recommended_ranks.get(prediction)
-                    if rank is not None and rank < k:
-                        data.append(float(rank))
-                        rows.append(user)
-                        cols.append(prediction)
-
-            ranks = sp.csr_matrix((data, (rows, cols)), shape=test_interactions.shape)
-
-            return ranks
-
-    return PopularityModel()
 
 def parse_arguments():
     """Parse job arguments."""
@@ -156,44 +57,6 @@ def parse_arguments():
         required=True
     )
 
-    # hyper params for model
-    parser.add_argument(
-        '--latent_factors',
-        type=int,
-        help='Number of latent factors',
-    )
-    parser.add_argument(
-        '--num_iters',
-        type=int,
-        help='Number of iterations for alternating least squares factorization',
-    )
-    parser.add_argument(
-        '--regularization',
-        type=float,
-        help='L2 regularization factor',
-    )
-    parser.add_argument(
-        '--unobs_weight',
-        type=float,
-        help='Weight for unobserved values',
-    )
-    parser.add_argument(
-        '--wt_type',
-        type=int,
-        help='Rating weight type (0=linear, 1=log)',
-        default=wals.LINEAR_RATINGS
-    )
-    parser.add_argument(
-        '--feature_wt_factor',
-        type=float,
-        help='Feature weight factor (linear ratings)',
-    )
-    parser.add_argument(
-        '--feature_wt_exp',
-        type=float,
-        help='Feature weight exponent (log ratings)',
-    )
-
     # other args
     parser.add_argument(
         '--output-dir',
@@ -210,12 +73,6 @@ def parse_arguments():
         default=False,
         action='store_true',
         help='Switch to turn on or off hyperparam tuning'
-    )
-    parser.add_argument(
-        '--use-optimized',
-        default=False,
-        action='store_true',
-        help='Use optimized hyperparameters'
     )
 
     args = parser.parse_args()
@@ -246,15 +103,12 @@ def parse_arguments():
     task_data = env.get('task') or {'type': 'master', 'index': 0}
 
     # update default params with any args provided to task
-    params = model.DEFAULT_PARAMS
+    params = model.OPTIMIZED_PARAMS
     params.update({k: arg for k, arg in arguments.items() if arg is not None})
-    if args.use_optimized:
-        params.update(model.OPTIMIZED_PARAMS)
 
     params.update(task_data)
     params.update({'output_dir': output_dir})
     params.update({'job_name': job_name})
-    params.update({'wt_type': wals.LOG_RATINGS})
 
     return params
 
